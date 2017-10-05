437e724a0d5b61f9e4f352dcb1db8e23528ef21b
Nurila <nurila@mail.ru>
2017-10-05 22:56:53 +0600

    Nurila

--- scr/kz/kaznitu/lessons/Hello.java
+++ scr/kz/kaznitu/lessons/Hello.java
@@ -6,5 +6,11 @@ public class Hello {
         System.out.println("Hello World");
         System.out.println("Бұл зертханалық жұмыстың мақсаты:");
         System.out.println("GitGub-пен жұмыс жасап уйрену:");
+
+        int a = MyMath.kvadrat( a:5);
+        int b = MyMath.kub(a:5) ;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 }

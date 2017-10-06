392c81df722db6a6b47890b7cfeea5303221c4c2
Nurila <nurila@mail.ru>
2017-10-06 10:57:20 +0600

    experiment1 бұтағында Hello.java және MyMath.java файлдарына өзгерістер жасалдыю

--- scr/kz/kaznitu/lessons/Hello.java
+++ scr/kz/kaznitu/lessons/Hello.java
@@ -12,5 +12,7 @@ public class Hello {
 
         System.out.println("a = " + a);
         System.out.println("b = " + b);
+
+        System.out.println(MyMath.max(a: 8, b:9));
     }
 }
--- scr/kz/kaznitu/lessons/MyMath.java
+++ scr/kz/kaznitu/lessons/MyMath.java
@@ -8,4 +8,20 @@ public class MyMath {
     public static int kub(int a){
         return a*a*a ;
     }
+    public static boolean isEven(int a){
+        return (a % 2 == 0) ;
+    }
+    public static int max(int a, int b){
+        if (a>= b)
+            return a;
+        else
+            return b ;
+    }
+    public static int min(int a, int b){
+        if (a>= b)
+            return a;
+        else
+            return b;
+    }
+
 }

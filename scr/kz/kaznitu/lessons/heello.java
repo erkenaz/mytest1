0c83fad7abce9067c7d64c0142e3decf21d7e679
Nurila <nurila@mail.ru>
2017-10-06 10:43:46 +0600

    nuri

---
+++ scr/kz/kaznitu/lessons/heello.java
@@ -0,0 +1,17 @@
+package kz.kaznitu.lessons;
+
+public class heello {package kz.kaznitu.lessons;
+
+        public static void main(String [] args) {
+            System.out.println("Hello World");
+            System.out.println("Бұл зертханалық жұмыстың мақсаты:");
+            System.out.println("GitGub-пен жұмыс жасап уйрену:");
+
+            int a = MyMath.kvadrat( a:5);
+            int b = MyMath.kub(a:5) ;
+
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+        }
+    }
+}
